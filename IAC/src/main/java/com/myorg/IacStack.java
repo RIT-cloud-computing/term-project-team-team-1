cf3512ebc4d91f2f65aaea7e57c1e313988cb0d2
Keenan Miller <43387387+keenanm500@users.noreply.github.com>
2021-11-17 15:34:58 -0500

    Update IacStack.java

--- IAC/src/main/java/com/myorg/IacStack.java
+++ IAC/src/main/java/com/myorg/IacStack.java
@@ -6,7 +6,6 @@ import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
 import software.amazon.awscdk.core.Duration;
-import software.amazon.awscdk.services.*;
 import software.amazon.awscdk.services.dynamodb.AttributeType;
 import software.amazon.awscdk.services.dynamodb.GlobalSecondaryIndexProps;
 import software.amazon.awscdk.services.dynamodb.LocalSecondaryIndexProps;
